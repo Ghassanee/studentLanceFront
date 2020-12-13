962108cfb3794dfca79eb12e0da454d3b7b95405
Ghassanee <ghassane20a@gamil.com>
2020-12-13 23:09:33 +0100

    ll

--- src/app/user/user-service.service.ts
+++ src/app/user/user-service.service.ts
@@ -18,6 +18,6 @@ export class UserServiceService {
     return this.httpClient.get(`https://shrouded-beyond-14016.herokuapp.com/StudentLance/JobOpening_User/${ref}`);
   }
   submitJob(job: JobUserPayload): Observable<any>{
-    return this.httpClient.post(`https://shrouded-beyond-14016.herokuapp.com/StudentLance/JobOpening_User/`, job);
+    return this.httpClient.post('https://shrouded-beyond-14016.herokuapp.com/StudentLance/JobOpening_User/', job);
   }
 }
