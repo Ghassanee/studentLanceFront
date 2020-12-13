e3aa64c7a5ca0588b1880e31a09658b6c6feeae8
Ghassanee <ghassane20a@gamil.com>
2020-12-13 23:02:20 +0100

    ll

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,15 +10,24 @@ import { UserServiceService } from './user-service.service';
 })
 export class UserComponent implements OnInit {
 
-  constructor(private cookieService: CookieService, private userService: UserServiceService) { }
   user: any;
   jobs: any;
   submittedJobs: any;
   panelOpenState: boolean;
   panelOpenState1: boolean;
   clicked: boolean;
+  error : boolean;
   jobUser: JobUserPayload;
+  constructor(private cookieService: CookieService, private userService: UserServiceService) { 
+    this.jobUser={
+      jobUserRef:'',
+      user:null,
+      jobOpening:null
+    }
+  }
+
   ngOnInit(): void {
+    this.error = false;
     this.clicked = false;
     this.panelOpenState = false;
     this.panelOpenState1 = false;
@@ -30,7 +39,12 @@ export class UserComponent implements OnInit {
     this.userService.getSubmittedJobs(this.user.userRef)
     .subscribe(data =>{ console.log(data);
       this.submittedJobs = data;
-    });
+    },
+    err => {
+      if(err.status == 404) {
+        this.error = true;
+      } 
+  });
   }
   submit(job: any){
     this.jobUser.jobOpening = job;
