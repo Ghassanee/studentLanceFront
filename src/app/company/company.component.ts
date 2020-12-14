17769d30685451afcd3d1b03fd89b25e9c458b9b
Ghassanee <ghassane20a@gamil.com>
2020-12-14 17:41:04 +0100

    lel

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -72,7 +72,7 @@ export class CompanyComponent implements OnInit {
   this.jobService.getJobs(this.company.companyRef)
   .subscribe(data =>{ 
     this.jobs = data;
-    data.array.forEach((j: any) => {
+    data.forEach((j: any) => {
       this.getUsersPerJob(j.jobOpeningRef);
     });
   });
